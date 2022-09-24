26ebd910d425e3c53002a77e73da9cd37cedc538
Raul Vallverdu <raulvallverdu.informatica@gmail.com>
2022-09-24 11:19:56 +0200

    trackeado

---
+++ LPE clase 23-09-2022.R
@@ -0,0 +1,22 @@
+
+# id script ---------------------------------------------------------------
+
+#Lenguajes de programacion estadistica
+#Profesor: Christian Sucuzhana Arevalo
+#Alumno: Raul Vallverdu Esteban ; exp: 22060707
+#Hands on 01
+
+
+# Ingest function ---------------------------------------------------------
+
+#crtl +  l = clean console
+#crtl + shift + r = new section
+
+
+
+# loading libs ------------------------------------------------------------
+
+install.packages("tidyverse","httr","janitor")
+
+
+
